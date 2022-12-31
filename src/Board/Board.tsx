846f2b4cbff1b6f9cac587db7d6e548fbd00c07d
ibrod1983 <ibrod1983@gmail.com>
2022-12-31 12:37:24 +0200

    moved instructions to a separate component

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -9,6 +9,7 @@ import { BsDownload } from 'react-icons/bs'
 import { Player as I_Player } from '../Player/types';
 import { defaultBoard, getRandomInt } from './utils';
 import Player from '../Player/Player'
+import Instructions from './Instructions';
 
 const jsonFromPersistence = localStorage.getItem('diceGamePersistence')
 
@@ -135,25 +136,7 @@ function Board() {
         </div>
 
       </div>
-      <div className="instructions">
-        <h2>Instructions</h2>
-        <p>
-          The game has 2 players, playing in rounds.
-          In each turn, a player rolls 2 dices as many times as he wishes.
-          Each result will get added to his round’s score.
-          But if the player rolls a double six all his round’s score gets lost.
-          After that, its the next player’s turn.
-        </p>
-        <p>
-          A player can choose to ‘Hold’, which means that his round’s score
-          gets added to his global score. After that, its the next players turn.
-          The first player to reach 100 points wins.
-        </p>
-        <p>
-          Pressing "new game" will reset the score. Pressing reset will also reset the win counter of each player.
-        </p>
-
-      </div>
+     <Instructions></Instructions>
     </>
 
   );
---
+++ src/Board/Instructions.tsx
@@ -0,0 +1,39 @@
+
+import './style.css';
+
+
+const Instructions = () => {
+    return (
+        <div className="instructions">
+            <h2>Instructions</h2>
+            <p>
+                The game has 2 players, playing in rounds.
+                In each turn, a player rolls 2 dices as many times as he wishes.
+                Each result will get added to his round’s score.
+                But if the player rolls a double six all his round’s score gets lost.
+                After that, its the next player’s turn.
+            </p>
+            <p>
+                A player can choose to ‘Hold’, which means that his round’s score
+                gets added to his global score. After that, its the next players turn.
+                The first player to reach 100 points wins.
+            </p>
+            <p>
+                Pressing "new game" will reset the score. Pressing reset will also reset the win counter of each player.
+            </p>
+
+        </div>
+    );
+}
+
+export default Instructions;
+
+
+
+
+
+
+
+
+
+
